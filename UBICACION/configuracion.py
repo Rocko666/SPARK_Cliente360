036e940487061d7f501083872a8bedd1031772f1
Crist_Ortiz <60772656+Rocko666@users.noreply.github.com>
2022-09-25 19:44:40 -0500

    creacion de archivos spark, sigo en transformacion

---
+++ UBICACION/configuracion.py
@@ -0,0 +1,85 @@
+# Nombre cola ejecucion
+val_cola_ejecucion = 'default'
+
+# Campo particion tabla
+# particion = 'fechaproceso'
+
+# Generales
+##valor cambiado a TRUE para validaciones
+val_se_calcula = True
+##valor cambiado del prefijo, 
+val_prefijo_tabla = 'BIGD677'
+
+## valor cambiado en correcciones de valores
+val_base_temporales = 'db_desarrollo2021'###'db_temporales'
+val_base_reportes = 'db_desarrollo2021' ### 'db_reportes'
+val_base_ipaccess_consultas = 'db_ipaccess'
+
+# Tablas Finales
+
+val_otc_t_cambio_plan_bi = 'otc_t_cambio_plan_bi'
+val_plan = 'plan'
+
+# Tablas Temporales
+
+val_mksharevozdatos_90 = 'mksharevozdatos_90'
+val_franja_horaria='GLOBAL'
+
+val_cambios_numero_2 = 'otc_t_cambios_numero_2'
+val_parque_actual_final = 'parque_actual_final'
+val_parque_anterior_1 = 'parque_anterior_1'
+val_parque_anterior_2 = 'parque_anterior_2'
+val_parque_anterior = 'parque_anterior'
+val_otc_t_ctl_pos_usr_nc = 'otc_t_ctl_pos_usr_nc'
+val_otc_t_ctl_pos_usr_nc_cambio_plan_union = 'otc_t_ctl_pos_usr_nc_cambio_plan_union'
+val_usuario = 'usuario'
+val_tmp_cambio_plan_1_1_2 = 'tmp_cambio_plan_1_1_2'
+val_tmp_cambio_plan_1_1 = 'tmp_cambio_plan_1_1'
+val_validar_pa = 'cp_validar_pa'
+val_validar_nc = 'cp_validar_nc'
+val_cambio_plan = 'cambio_plan'
+val_tmp_cambio_plan_fnl = 'tmp_cambio_plan_fnl'
+val_override_actual = 'override_actual'
+val_override_planes_ant = 'override_planes_ant'
+val_override_anterior = 'override_anterior'
+val_otc_t_descuentos_planes_act = 'otc_t_descuentos_planes_act'
+val_descuento_actual = 'descuento_actual'
+val_tmp_ov_desc_plan_new = 'tmp_ov_desc_plan_new'
+val_descuento_anterior = 'descuento_anterior'
+val_cambio_plan_fnl_int = 'cambio_plan_fnl_int'
+
+
+# Para almacenar la temporal en HIVE
+val_genera_temp_mksharevozdatos_90 = False
+val_genera_temp_plan = False
+val_genera_parque_actual_1 = False
+val_genera_parque_actual_2 = False
+val_genera_parque_actual = False
+val_genera_parque_actual_3 = False
+val_genera_parque_actual_1 = False
+val_genera_parque_actual_2 = False
+val_genera_parque_actual = False
+val_genera_cambios_numero_1 = False
+val_genera_cambios_numero_2 = False
+val_genera_parque_actual_final = False
+val_genera_parque_actual_final = False
+val_genera_parque_anterior_1 = False
+val_genera_parque_anterior_2 = False
+val_genera_parque_anterior = False
+val_genera_otc_t_ctl_pos_usr_nc = True
+val_genera_ctl_pos_usr_nc_cambio_plan_union = False
+val_genera_usuario = False
+val_genera_tmp_cambio_plan_1_1_2 = False
+val_genera_tmp_cambio_plan_1_1 = False
+val_genera_validar_pa = False
+val_genera_validar_nc = False
+val_genera_cambio_plan = False
+val_genera_tmp_cambio_plan_fnl = False
+val_genera_override_actual =  False
+val_genera_override_planes_ant = False
+val_genera_override_anterior =  False
+val_genera_otc_t_descuentos_planes_act = False
+val_genera_descuento_actual = False
+val_genera_tmp_ov_desc_plan_new = False
+val_genera_descuento_anterior = False
+val_genera_cambio_plan_fnl_int = False
